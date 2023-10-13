97f41129bcfca98d99fad61e29afed70eb91f192
Mohamed Ashraf <88565528+Mohamedashraf2005@users.noreply.github.com>
2023-10-13 19:45:42 +0300

    Add files via upload

---
+++ Char_To_ASCII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	char x;
+	cout << "Enter The Character You Want to Convert Into ASCII Value \n";	
+	cin >> x;
+	cout << "The ASCII Value is " << int(x) ; 
+	
+	
+	return 0;
+}
+
+
