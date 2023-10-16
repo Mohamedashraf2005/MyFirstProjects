97e42e2386aa15e8161ef93868801b4049ada98a
Mohamed Ashraf <88565528+Mohamedashraf2005@users.noreply.github.com>
2023-10-17 00:46:36 +0300

    Add files via upload

---
+++ Perimeter_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int length; 
+	int Width ;
+	cout <<"==Perimeter of a Rectangle Calculator==\n";	
+	cout <<"Enter the length \n";
+	cin >> length;
+	cout <<"Enter the Width \n";
+	cin >> Width;
+	cout <<"The Perimeter of a Rectangle is " << (length + Width ) * 2 ;	
+	return 0;
+}
+
+
